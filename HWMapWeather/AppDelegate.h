0d448f331e58d5c8cd78cb952d8584046738ec28
Perets <perets@macbook-perets.local>
2016-02-06 13:12:48 +0200

    Initial Commit

---
+++ HWMapWeather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HWMapWeather
+//
+//  Created by Perets on 06.02.16.
+//  Copyright © 2016 Perets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HWMapWeather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HWMapWeather
+//
+//  Created by Perets on 06.02.16.
+//  Copyright © 2016 Perets. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
